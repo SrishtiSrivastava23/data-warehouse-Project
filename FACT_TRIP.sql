a3de82489a63f453acfa6638598dfc2d2c30d1b0
Srishti Srivastava <146706947+SrishtiSrivastava23@users.noreply.github.com>
2026-05-06 15:35:36 +0530

    Add calculation for rider age in fact_trip table

--- FACT_TRIP.sql
+++ FACT_TRIP.sql
@@ -6,7 +6,7 @@ WITH (
 )
 AS
 SELECT
- t.trip_id,
+    t.trip_id,
     t.rider_id,
     t.start_station_id,
     t.end_station_id,
@@ -14,7 +14,17 @@ SELECT
     CAST(CONVERT(VARCHAR(8), CAST(t.start_at AS DATE), 112) AS INT) AS date_key,
 
     DATEDIFF(MINUTE, t.start_at, t.ended_at) AS trip_duration_minutes,
-    DATEDIFF(YEAR, r.birthday, t.start_at) AS rider_age
+
+    DATEDIFF(YEAR, r.birthday, t.start_at)
+    -
+
+    CASE 
+        WHEN MONTH(r.birthday) > MONTH(t.start_at)
+        OR (MONTH(r.birthday) = MONTH(t.start_at)
+            AND DAY(r.birthday) > DAY(t.start_at))
+        THEN 1 ELSE 0
+    END AS rider_age
+
 FROM ext_trip t
 JOIN ext_rider r
     ON t.rider_id = r.rider_id;
