321fc507539f07f35132b96a30a9672b9575b9b3
Srishti Srivastava <146706947+SrishtiSrivastava23@users.noreply.github.com>
2026-05-06 15:34:37 +0530

    Revise dim_date table to include additional date sources
    Updated the dim_date table creation to include dates from both start_at and ended_at columns, as well as from ext_payment.

--- dim_date.sql
+++ dim_date.sql
@@ -5,13 +5,23 @@ WITH (
     FILE_FORMAT = SynapseDelimitedTextFormat
 )
 AS
+WITH ALLDates AS
+(
+SELECT CAST(start_at AS DATE) AS date_val FROM dbo.ext_trip
+UNION
+SELECT CAST(ended_at AS DATE) AS date_val FROM dbo.ext_trip
+UNION
+SELECT CAST([date] AS DATE) AS date_val FROM dbo.ext_payment
+)
+
 SELECT DISTINCT
-    CAST(CONVERT(VARCHAR(8), CAST(start_at AS DATE),112) AS INT) AS date_key,
-    CAST(start_at AS DATE) AS full_date,
-    DATEPART(DAY, start_at) AS day,
-    DATEPART(MONTH, start_at) AS month,
-    DATEPART(YEAR, start_at) AS year,
-    DATEPART(QUARTER, start_at) AS quarter,
-    DATEPART(WEEKDAY, start_at) AS weekday
-FROM ext_trip;
+    CAST(CONVERT(VARCHAR(8), date_val ,112) AS INT) AS date_key,
+    date_val AS full_date,
+    DATEPART(DAY, date_val) AS day,
+    DATEPART(MONTH, date_val) AS month,
+    DATEPART(YEAR, date_val) AS year,
+    DATEPART(QUARTER, date_val) AS quarter,
+    DATEPART(WEEKDAY, date_val) AS weekday
+FROM AllDates
+WHERE date_val IS NOT NULL;
 GO
